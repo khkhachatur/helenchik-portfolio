1a77e9a6dbc1e1bf84538f16d0d178dd34040108
khkhachatur <khachatryankhachatur57@gmail.com>
2026-03-15 14:13:18 +0100

    support button in navbar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -50,6 +50,14 @@ export default function Navbar() {
 
   return (
     <>
+    <a 
+        href="/support"
+        className="fixed top-6 left-6 md:top-10 md:left-10 z-[80] group flex items-center gap-3 mix-blend-difference"
+      >
+        <div className="text-[#DC2626] hover:text-white hover:bg-[#DC2626] transition-all duration-300 font-mono uppercase tracking-widest text-xs md:text-sm bg-black/20 px-4 py-2 backdrop-blur-md border border-[#DC2626]/50 shadow-[0_0_15px_rgba(220,38,38,0.2)]">
+          [ HELP HELEN STUDY IN EUROPE ]
+        </div>
+      </a>
       <button 
         onClick={() => setIsOpen(true)}
         className="fixed top-6 right-6 md:top-10 md:right-10 z-[80] group flex items-center gap-3 mix-blend-difference"
